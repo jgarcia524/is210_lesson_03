b473478b492303dd853f516e5a619b93b1e86c1a
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-13 11:15:38 -0400

    removed syntax error

--- task_02.py
+++ task_02.py
@@ -11,7 +11,7 @@ elif BLOODPRESSURE > 119 and BLOODPRESSURE <= 139:
     BP_STATUS = "Warning"
 elif BLOODPRESSURE > 139 and BLOODPRESSURE <= 159:
     BP_STATUS = "High"
-elif: BLOODPRESSURE >= 160:
+elif BLOODPRESSURE >= 160:
     BP_STATUS = "Emergency"
 
 
