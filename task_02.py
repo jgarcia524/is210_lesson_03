a9e861c164c7cb356416ff7497e6b061ad450673
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-13 11:08:13 -0400

    changed high range

--- task_02.py
+++ task_02.py
@@ -9,7 +9,7 @@ elif BLOODPRESSURE > 89 and BLOODPRESSURE <= 119:
     BP_STATUS = "Ideal"
 elif BLOODPRESSURE > 119 and BLOODPRESSURE <= 139:
     BP_STATUS = "Warning"
-elif BLOODPRESSURE > 139 and BLOODPRESSURE <= 149:
+elif BLOODPRESSURE > 139 and BLOODPRESSURE <= 159:
     BP_STATUS = "High"
 else:
     BP_STATUS = "Emergency"
