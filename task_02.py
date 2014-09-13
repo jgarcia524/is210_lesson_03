eb1525d68523d92dd8c5ef729ae2848a224d42ed
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-13 11:05:03 -0400

    completed task

---
+++ task_02.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+BLOODPRESSURE = int(raw_input("What is your blood pressure?" ))
+
+if BLOODPRESSURE <= 89:
+    BP_STATUS = "Low"
+elif BLOODPRESSURE > 89 and BLOODPRESSURE <= 119:
+    BP_STATUS = "Ideal"
+elif BLOODPRESSURE > 119 and BLOODPRESSURE <= 139:
+    BP_STATUS = "Warning"
+elif BLOODPRESSURE > 139 and BLOODPRESSURE <= 149:
+    BP_STATUS = "High"
+else:
+    BP_STATUS = "Emergency"
+
+Status = "Your blood pressure status is {}."
+print Status.format(BP_STATUS)
