3da40415d9096b79582dfce84bef53b0cda38e91
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-13 12:21:03 -0400

    made changes

--- task_04.py
+++ task_04.py
@@ -6,10 +6,8 @@ USERDAY = raw_input("What day of the week is it today?")
 TODAY = USERDAY.lower()
 DAY = TODAY[0:3]
 TIME = int(raw_input("What time is it? (exclude the colon symbol in your response.)"))
-SNOOZE = "more sleep!"
 
-if DAY == "sat" or DAY == "sun" or TIME <= 600:
-    SNOOZE == True
-else:
-    SNOOZE == False
+SNOOZE = DAY == "sat" or DAY == "sun" or TIME <= 600
+
+if SNOOZE == False:
     print "Beep! Beep! Beep! Beep! Beep!"
