6f9a7d32b3c6d6bd383c990c172af3be6a1bfc22
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-14 08:58:49 -0400

    pylint changes

--- task_03.py
+++ task_03.py
@@ -1,22 +1,30 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Helps deciding on paint colors"""
 
 BASE = raw_input("Choose a base color: Seattle Gray or Manatee ")
 
 if BASE == "Seattle Gray":
-    ACCENT = raw_input("Choose an accent color: Ceramic Glaze or Cumulus Nimbus ")
+    ACCENT = raw_input(
+        "Choose an accent color:Ceramic Glaze or Cumulus Nimbus ")
     if ACCENT== "Ceramic Glaze":
-        HIGHLIGHT = raw_input("Choose a highlight color: Basically White or White ")
+        HIGHLIGHT = raw_input(
+            "Choose a highlight color: Basically White or White ")
     elif ACCENT == "Cumulus Nimbus":
-        HIGHLIGHT = raw_input("Choose a highlight color: Off-White or Paper White ")
+        HIGHLIGHT = raw_input(
+            "Choose a highlight color: Off-White or Paper White ")
 
 else:
     if BASE == "Manatee":
-        ACCENT = raw_input("Choose an accent color: Platinum Mist or Spartan Sage ")
+        ACCENT = raw_input(
+            "Choose an accent color: Platinum Mist or Spartan Sage ")
         if ACCENT == "Platinum Mist":
-            HIGHLIGHT = raw_input("Choose a highlight color:Bone White or Just White ")
+            HIGHLIGHT = raw_input(
+                "Choose a highlight color:Bone White or Just White ")
         elif ACCENT == "Spartan Sage":
-            HIGHLIGHT = raw_input("Choose an accent color: Fractal White or Not White ")
+            HIGHLIGHT = raw_input(
+                "Choose an accent color: Fractal White or Not White ")
 
-SELECTION = "Your color selections are: {0} for the base color, {1} for the accent color, and {2} for the highlight color."
+SELECTION = ("Your color selections are: {0} for the base color,"
+             " {1} for the accent color, and {2} for the highlight color.")
 print(SELECTION.format(BASE, ACCENT, HIGHLIGHT))
