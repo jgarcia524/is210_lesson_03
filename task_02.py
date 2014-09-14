fcc823928f0a5c499e2ce6918c6a70c61c6730b2
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-14 09:13:13 -0400

    forgot equal signs

--- task_02.py
+++ task_02.py
@@ -6,11 +6,11 @@ BLOODPRESSURE = int(raw_input("What is your blood pressure?"))
 
 if BLOODPRESSURE <= 89:
     BP_STATUS = "Low"
-elif BLOODPRESSURE > 89 and BLOODPRESSURE <= 119:
+elif BLOODPRESSURE >= 89 and BLOODPRESSURE <= 119:
     BP_STATUS = "Ideal"
-elif BLOODPRESSURE > 119 and BLOODPRESSURE <= 139:
+elif BLOODPRESSURE >= 119 and BLOODPRESSURE <= 139:
     BP_STATUS = "Warning"
-elif BLOODPRESSURE > 139 and BLOODPRESSURE <= 159:
+elif BLOODPRESSURE >= 139 and BLOODPRESSURE <= 159:
     BP_STATUS = "High"
 elif BLOODPRESSURE >= 160:
     BP_STATUS = "Emergency"
