5be7e453293b7d75542a59cca1b37a586e5a0fc8
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-14 09:09:52 -0400

    pylint changes

--- task_03.py
+++ task_03.py
@@ -7,7 +7,7 @@ BASE = raw_input("Choose a base color: Seattle Gray or Manatee ")
 if BASE == "Seattle Gray":
     ACCENT = raw_input(
         "Choose an accent color:Ceramic Glaze or Cumulus Nimbus ")
-    if ACCENT== "Ceramic Glaze":
+    if ACCENT == "Ceramic Glaze":
         HIGHLIGHT = raw_input(
             "Choose a highlight color: Basically White or White ")
     elif ACCENT == "Cumulus Nimbus":
@@ -27,4 +27,4 @@ else:
 
 SELECTION = ("Your color selections are: {0} for the base color,"
              " {1} for the accent color, and {2} for the highlight color.")
-print(SELECTION.format(BASE, ACCENT, HIGHLIGHT))
+print SELECTION.format(BASE, ACCENT, HIGHLIGHT)
