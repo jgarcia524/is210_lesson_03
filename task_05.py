9ca00b4813feff21326ad3aa227739d1bdbe67d9
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-14 09:26:20 -0400

    changed variables to uppercase for pylint

--- task_05.py
+++ task_05.py
@@ -5,54 +5,54 @@
 from decimal import Decimal
 NAME = raw_input("What is your name?")
 P = int(raw_input("What is the amount of your principal (the amount being borrowed)?"))
-t = int(raw_input("For how many years is this loan being borrowed?"))
-Qual = raw_input("Are you prequalified for this loan?")
-q = Qual.lower()
-Q = q[0]
-r = None
+T = int(raw_input("For how many years is this loan being borrowed?"))
+QUAL = raw_input("Are you prequalified for this loan?")
+QU = QUAL.lower()
+Q = QU[0]
+R = None
 
 if P >=0 and P <= 199999:
-    if t >= 1 and t <= 15:
+    if T >= 1 and T <= 15:
         if Q == "y":
-            r = "0.0363"
+            R = "0.0363"
         elif Q == "n":
-            r = "0.0465"
-    elif t >= 16 and t <= 20:
+            R = "0.0465"
+    elif T >= 16 and T <= 20:
         if Q == "y":
-            r = "0.0404"
+            R = "0.0404"
         elif Q == "n":
-            r = "0.0498"
-    elif t >= 21 and t <= 30:
+            R = "0.0498"
+    elif T >= 21 and T <= 30:
         if Q == "y":
-            r = "0.0577"
+            R = "0.0577"
         elif Q == "n":
-            r = "0.0639"
+            R = "0.0639"
 elif P >= 200000 and P <= 999999:
-    if t >= 1 and t <= 15:
+    if T >= 1 and T <= 15:
         if Q == "y":
-            r = "0.0302"
+            R = "0.0302"
         elif Q == "n":
-            r = "0.0398"
-    elif t >= 16 and t <= 20:
+            R = "0.0398"
+    elif T >= 16 and T <= 20:
         if Q == "y":
-            r = "0.0327"
+            R = "0.0327"
         elif Q == "n":
-            r = "0.0408"
-    elif t >= 21 and t <= 30:
+            R = "0.0408"
+    elif T >= 21 and T <= 30:
         if Q == "y":
-            r = "0.0466"
+            R = "0.0466"
 elif P >= 1000000:
-    if t >= 1 and t <= 15:
+    if T >= 1 and T <= 15:
         if Q == "y":
-            r = "0.0205"
-    elif t >= 16 and t <= 20:
+            R = "0.0205"
+    elif T >= 16 and T <= 20:
         if Q == "y":
-            r = "0.0262"
-n = 12
-if r == None:
+            R = "0.0262"
+N = 12
+if R is None:
     TOTAL = None
 else:
-    FINAL = P*((1+(Decimal(r))/n))**(n*t)
-    total = round(FINAL)
-    TOTAL = int(total)
+    FINAL = P*((1+(Decimal(R))/N))**(N*T)
+    TOT = round(FINAL)
+    TOTAL = int(TOT)
 print TOTAL
