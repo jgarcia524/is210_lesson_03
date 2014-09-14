f577de02deebbe9e843e18093e9e21f6a727985a
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-14 09:15:05 -0400

    pylint changes

--- task_04.py
+++ task_04.py
@@ -1,13 +1,14 @@
-#1!user/bin/env python
+#!user/bin/env python
 # -*- coding: utf-8 -*-
 """Wake up alarm!"""
 
 USERDAY = raw_input("What day of the week is it today?")
 TODAY = USERDAY.lower()
 DAY = TODAY[0:3]
-TIME = int(raw_input("What time is it? (exclude the colon symbol in your response.)"))
+TIME = int(raw_input(
+    "What time is it? (exclude the colon symbol in your response.)"))
 
 SNOOZE = DAY == "sat" or DAY == "sun" or TIME <= 600
 
-if SNOOZE == False:
+if SNOOZE != True:
     print "Beep! Beep! Beep! Beep! Beep!"
